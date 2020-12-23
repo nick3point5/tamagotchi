7778e273e98996bc2f3312496c43cf2b869d6418
kennytrinh <kenny.trinh4444@gmail.com>
2020-12-23 13:09:42 -0600

    sleeping/animation changes/pet visiblity

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//ANCHOR define classes
+
 class interObj {
   constructor(x, y, width, height, element) {
     this.x = x;
@@ -17,6 +19,37 @@ class interObj {
     };
     this.element = element;
   }
+  collision(hurtbox){
+    if (
+        this.x + this.width > hurtbox.x &&
+        this.x < hurtbox.x + hurtbox.width &&
+        this.y + this.height > hurtbox.y &&
+        this.y < hurtbox.y + hurtbox.width
+    ) {
+        return true
+    }
+    return false
+  };
+  moveFunction() {
+    if (this.move.up && this.y - this.speed >= this.ymin) {
+      this.y -= this.speed;
+      this.element.style.top = `${this.y}px`;
+    }
+    if (this.move.dn && this.y + this.speed <= this.ymax) {
+      this.y += this.speed;
+      this.element.style.top = `${this.y}px`;
+    }
+    if (this.move.lf && this.x - this.speed >= this.xmin) {
+      this.x -= this.speed;
+      this.element.style.left = `${this.x}px`;
+      this.element.style.transform = 'scaleX(-1)'
+    }
+    if (this.move.ri && this.x + this.speed <= this.xmax) {
+      this.x += this.speed;
+      this.element.style.left = `${this.x}px`;
+      this.element.style.transform = 'scaleX(1)'
+    }
+  }
 }
 
 class foodClass extends interObj{
@@ -26,25 +59,21 @@ class foodClass extends interObj{
 }
 
 class petClass extends interObj {
-  constructor() {
-    super(
-      0,
-      342,
-      $("#pet")[0].clientWidth,
-      $("#pet")[0].clientHeight,
-      $("#pet")[0]
-    );
+  constructor(x,y,width,height,element) {
+    super(x,y,width,height,element);
     this.belly = 10;
     this.energy = 10;
     this.fun = 10;
     this.age = 0;
-    this.appetite = 0.05;
+    this.appetite = 0.01;
     this.stamina = 0.01;
     this.attention = 0.01;
     this.speed = 5;
-    this.conditions = ['Happy','hungry','sleepy','bored']
-    this.currentState = 'Happy'
-
+    this.conditions = ['happy','hungry','sleepy','bored','sleeping']
+    this.currentState = 'happy'
+    this.spriteFrame = 0
+    this.spriteFrameRate = 1
+    this.spriteFrameMax = 9
 
     this.move = {
       up: false,
@@ -53,47 +82,47 @@ class petClass extends interObj {
       lf: false,
     };
 
-    this.awake = true;
     this.alive = false;
   }
-}
-
-function move(obj) {
-  if (obj.move.up && obj.y - obj.speed >= obj.ymin) {
-    obj.y -= obj.speed;
-    obj.element.style.top = `${obj.y}px`;
-  }
-  if (obj.move.dn && obj.y + obj.speed <= obj.ymax) {
-    obj.y += obj.speed;
-    obj.element.style.top = `${obj.y}px`;
-  }
-  if (obj.move.lf && obj.x - obj.speed >= obj.xmin) {
-    obj.x -= obj.speed;
-    obj.element.style.left = `${obj.x}px`;
-  }
-  if (obj.move.ri && obj.x + obj.speed <= obj.xmax) {
-    obj.x += obj.speed;
-    obj.element.style.left = `${obj.x}px`;
-  }
-}
-
-function petAi() {
-    if(pet.currentState === 'Happy'){
-        statefine()
+  spriteFile(){
+    let imgURL = ''
+    if(this.currentState === pet.conditions[4]){
+      return `assets/sprites/sleeping/${this.spriteFrame}.png`
+    } else{
+      return `assets/sprites/walking/${this.spriteFrame}.png`
     }
-    if(pet.currentState === 'hungry'){
+  }
+  drawSprite() {
+    if (this.spriteFrame > this.spriteFrameMax){
+      this.spriteFrame = 0
+    }
+    this.element.setAttribute('src', this.spriteFile())
+  
+    this.spriteFrame++
+  }
+}
+
+//ANCHOR pet behaviors
+function petAi() {
+    if(pet.currentState === pet.conditions[0]){
+        stateHappy()
+    }
+    if(pet.currentState === pet.conditions[1]){
         stateHungry()
     }
-    if(pet.currentState === 'sleepy'){
+    if(pet.currentState === pet.conditions[2]){
         stateSleepy()
     }
-    if(pet.currentState === 'bored'){
+    if(pet.currentState === pet.conditions[3]){
         stateBored()
     }
+    if(pet.currentState === pet.conditions[4]){
+        stateSleeping()
+  }
 
 }
 
-function statefine(){
+function stateHappy(){
     pet.speed = 5
     if (pet.x >= pet.xmax - 4) {
         pet.move.ri = false;
@@ -103,6 +132,8 @@ function statefine(){
         pet.move.ri = true;
         pet.move.lf = false;
     }
+    pet.spriteFrameMax = 9
+    pet.spriteFrameRate = 2
 }
 function stateHungry(){
     pet.speed = 10
@@ -122,13 +153,14 @@ function stateHungry(){
         pet.move.dn = true;
     }
     
-    if(collision(pet,food)){
+    if(pet.collision(food)){
         pet.belly=10
         $('#food').addClass('hidden')
         food.x = -food.width
         food.y = food.ymax + food.height
     }
-
+    pet.spriteFrameMax = 9
+    pet.spriteFrameRate = 1
 }
 function stateSleepy(){
     pet.speed = 1
@@ -140,12 +172,25 @@ function stateSleepy(){
         pet.move.ri = true;
         pet.move.lf = false;
     }
+    pet.spriteFrameMax = 9
+    pet.spriteFrameRate = 3
 }
 function stateBored(){
 
 }
 
+function stateSleeping() {
+    pet.speed = 0
+    pet.move.up = false
+    pet.move.ri = false
+    pet.move.dn = false
+    pet.move.lf = false
+    pet.spriteFrameMax = 4
+    pet.spriteFrameRate = 3
+}
 
+
+//ANCHOR UI buttons
 function feed() {
     createFood();
 }
@@ -160,7 +205,7 @@ function createFood() {
 
 function sleep() {
   if (pet.energy < 10) {
-    pet.energy = 10;
+    pet.currentState = pet.conditions[4];
   }
 }
 
@@ -175,38 +220,13 @@ function play() {
   }
 }
 
-function assignEvents() {
-  $("#feedBtn").on("click", feed);
-  $("#sleepBtn").on("click", sleep);
-  $("#playBtn").on("click", play);
-  if (!pet.alive) {
-      $('.notification').on('click',game)
-  }
-}
-
-function collision(hitbox,hurtbox){
-    if (
-        hitbox.x + hitbox.width > hurtbox.x &&
-        hitbox.x < hurtbox.x + hurtbox.width &&
-        hitbox.y + hitbox.width > hurtbox.y &&
-        hitbox.y < hurtbox.y + hurtbox.width
-    ) {
-        return true
-    }
-    return false
-}
-
-function getRand(max, min) {
-    let num = Math.random() * (max + 1 - min) + min - 1;
-    num = Math.ceil(num);
-    return num;
-  }
-
+//ANCHOR update each frame
 function update() {
   uiUpdate();
   petUpdate();
-  move(pet);
+  pet.moveFunction();
   petAi();
+  frame++
 }
 
 function uiUpdate() {
@@ -224,7 +244,6 @@ function gaugeUi(belly,energy,fun) {
   funEl.attr('src',`assets/Bars/fun/pixil-frame-0 (${fun}).png`)
 }
 
-
 function petUpdate() {
   pet.belly -= pet.appetite;
   pet.energy -= pet.stamina;
@@ -234,10 +253,25 @@ function petUpdate() {
     pet.alive = false;
   }
   conditionCheck()
-
+  if(!(frame%pet.spriteFrameRate)){
+    pet.drawSprite()
+  }
 }
 
 function conditionCheck() {
+  if (pet.currentState === 'sleeping'){
+    if(pet.energy<10){
+      pet.belly += pet.appetite;
+      pet.energy += 0.2+pet.stamina;
+      pet.fun += pet.attention;
+      if (pet.energy > 10) {
+        pet.energy = 10
+        pet.currentState = pet.conditions[0]
+      }
+      return
+    } 
+    
+  } else {
     if (pet.belly>=5 && pet.energy>=5 && pet.fun>=5) {
         pet.currentState=pet.conditions[0]
     } else
@@ -250,24 +284,18 @@ function conditionCheck() {
     if (pet.fun < 5 && pet.fun<=pet.energy && pet.fun<=pet.belly) {
         pet.currentState=pet.conditions[3]
     }
+  }
 }
 
-function gameover() {
-    mesEl.text(`${$("#name")[0].value} has died`)
-  $("#music")[0].pause();
-  $("#music")[0].currentTime = 0;
-}
+//ANCHOR function utility
 
-function Init() {
-  pet.alive = true;
-  pet.belly = 10;
-  pet.energy = 10;
-  pet.fun = 10;
-  pet.age = 0;
-  pet.x = 0;
-  pet.y = pet.ymax;
-  $("#music")[0].play();
-}
+function getRand(max, min) {
+    let num = Math.random() * (max + 1 - min) + min - 1;
+    num = Math.ceil(num);
+    return num;
+  }
+
+//ANCHOR metagame functions
 
 function game() {
   Init();
@@ -284,12 +312,61 @@ function game() {
 
 }
 
+function gameover() {
+    mesEl.text(`${$("#name")[0].value} has died`)
+  $("#music")[0].pause();
+  $("#music")[0].currentTime = 0;
+  $("#pet").addClass('hidden')
+}
+
+function Init() {
+  pet.alive = true;
+  pet.belly = 10;
+  pet.energy = 10;
+  pet.fun = 10;
+  pet.age = 0;
+  pet.x = 0;
+  pet.y = pet.ymax;
+  $("#music")[0].play();
+  $("#pet").removeClass('hidden')
+}
+
+function pause() {
+  
+}
+
+function mute(){
+  playwin.mute = !playwin.mute
+  $('audio')[0].muted = !playwin.mute
+
+}
+
+function assignEvents() {
+  $("#feedBtn").on("click", feed);
+  $("#sleepBtn").on("click", sleep);
+  $("#playBtn").on("click", play);
+  $('#pauseBtn').on("click",pause)
+  $('#muteBtn').on("click",mute)
+  if (!pet.alive) {
+      $('.notification').on('click',game)
+  }
+}
+
+//ANCHOR global var and obj
+
 playwin = {
   height: 600,
   width: 800,
+  element: $('.play-space'),
+  mute: false
 };
 
-const pet = new petClass();
+const pet = new petClass(
+  0,
+  530,
+  $("#pet")[0].clientWidth,
+  $("#pet")[0].clientHeight,
+  $("#pet")[0]);
 pet.element.style.top = `${pet.y}px`;
 pet.element.style.left = `${pet.y}px`;
 
@@ -297,10 +374,13 @@ const food =  new foodClass(0,0,$('#food')[0]);
 food.x = -food.width
 food.y = food.ymax + food.height
 
+let frame = 0
+
 const bellyEl = $("#belly");
 const energyEl = $("#energy");
 const funEl = $("#fun");
 const ageEl = $("#age");
-const mesEl = $("#message")
+const mesEl = $("#message");
+const ctx = playwin.element.getContext
 
-assignEvents();
+assignEvents()
